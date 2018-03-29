9c5f1e9d10fb5e3fc36cb801059460ab6613bcdd
naveenmettu <naveenmettu@gmail.com>
2018-03-29 05:38:28 +0000

    second commit

--- chef_practice/learn.rb
+++ chef_practice/learn.rb
@@ -5,3 +5,24 @@ end
 service 'httpd' do
 	action [:enable, :start] 
 end
+
+file '/var/www/index.html' do
+	action :delete
+end
+
+file '/var/www/html/index.html' do
+	content 'Hello, World!, This is a new Line'
+	mode	'0755'
+	owner	'root'
+	group	'apache'
+end
+
+file '/etc/motd' do
+	content 'Welcome to the world of chef!'
+end
+
+execute 'test-command' do
+	command 'echo blah >> /etc/motd'
+	#only_if 'test -r /etc/motd'
+	only_if { File.exists?('/etc/motd') } 
+end
